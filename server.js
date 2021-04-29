8db6d2959b92f86ab76fd0d9c2f7276ab36c7ca1
Caroline Santos <caroline.santos.wb@gmail.com>
2021-04-29 08:52:22 -0300

    ajuste porta servidor para produção

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 
 app.use(routes);
-app.listen(3000, function(){
-    console.log('servidor rodando na porta 3000')
+const port = process.env.PORT || 3000 
+app.listen(port, function(){
+    console.log('Servidor rodando na porta 3000');
 })
